deceef2b0c2a0874041f97b883c15612419ed15c
viamagz <110997171+viamagz@users.noreply.github.com>
2022-08-15 09:56:39 +0700

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 			<div id="comments">
 <?php if ( post_password_required() ) : ?>
-				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'project-1' ); ?></p>
+				<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'be-good' ); ?></p>
 			</div>
 	<?php
 		return;
@@ -12,12 +12,12 @@
 			<?php
 			if ( 1 === get_comments_number() ) {
 				printf(
-					__( 'One Response to %s', 'project-1' ),
+					__( 'One Response to %s', 'be-good' ),
 					'<em>' . get_the_title() . '</em>'
 				);
 			} else {
 				printf(
-					_n( '%1$s Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'project-1' ),
+					_n( '%1$s Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'be-good' ),
 					number_format_i18n( get_comments_number() ),
 					'<em>' . get_the_title() . '</em>'
 				);
@@ -27,28 +27,28 @@
 
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&laquo;</span> Older', 'project-1' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer <span class="meta-nav">&raquo;</span>', 'project-1' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&laquo;</span> Older', 'be-good' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer <span class="meta-nav">&raquo;</span>', 'be-good' ) ); ?></div>
 			</div>
 	<?php endif; ?>
 
 			<ol class="commentlist">
 				<?php
-					wp_list_comments( array( 'callback' => 'project-1_comment' ) );
+					wp_list_comments( array( 'callback' => 'be-good_comment' ) );
 				?>
 			</ol>
 
 	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<div class="navigation">
-				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&laquo;</span> Older', 'project-1' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer <span class="meta-nav">&raquo;</span>', 'project-1' ) ); ?></div>
+				<div class="nav-previous"><?php previous_comments_link( __( '<span class="meta-nav">&laquo;</span> Older', 'be-good' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer <span class="meta-nav">&raquo;</span>', 'be-good' ) ); ?></div>
 			</div>
 	<?php endif; ?>
 
 	<?php
 	if ( ! comments_open() && get_comments_number() ) :
 		?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'project-1' ); ?></p>
+		<p class="nocomments"><?php _e( 'Comments are closed.', 'be-good' ); ?></p>
 	<?php endif; ?>
 
 <?php endif; ?>
